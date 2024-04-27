a67dcdb3f420e258056a30b8e3b8b7334cc2a387
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-04-27 21:48:12 +0530

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -25,34 +25,3 @@ resource "aws_subnet" "subnet2" {
    cidr_block = "${var.subnet2cidr}"
    vpc_id = "${aws_vpc.myvpc.id}"
 }
-
-resource "aws_security_group" "mysg" {
-   vpc_id = "${aws_vpc.myvpc.id}"
-   tags = {
-     Name = "Terra-SG"
-   }
-   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-   }
-   egress {
-    protocol = "tcp"
-    from_port = 0
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
-   }
-}
-
-resource "aws_instance" "myec2" {
-  ami = "ami-026b57f3c383c2eec"
-  instance_type = "${var.instancetype}"
-  subnet_id = "${aws_subnet.subnet1.id}"
-  security_groups = ["${aws_security_group.mysg.id}"]
-  tags = {
-      Name = "My Terra Inst"
-  }
-  key_name = "${var.keyname}"
-}
