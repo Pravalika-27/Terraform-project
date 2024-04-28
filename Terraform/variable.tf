a668dd1ce95181e4b812272ed514aa3854d51f94
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-04-28 18:45:50 +0530

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -1,11 +1,1 @@
-provider "aws" {
-   region = "${var.region}"
-   profile = "default"
-}
 
-resource "aws_vpc" "myvpc" {
-   tags = {
-     Name = "${var.vpcname}"
-   }
-   cidr_block = "${var.vpccidr}"
-}
