5e3ede305b60854388075d4e6511f747ef29c9e9
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-04-29 19:51:10 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,11 +2,3 @@ provider "aws" {
   region     = "ap-south-1"
   profile    = "default"
 }
-resource "aws_instance" "myec2" {
-  ami = "ami-026b57f3c383c2eec" 
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0e91a1f7fc88ec32c"
-  security_groups = ["sg-0b35d8431600bf13b"] 
-  tags = {
-      Name = "My Terra Inst"
-  }
