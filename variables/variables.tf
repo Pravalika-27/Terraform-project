df13e85206466e82c5b2cb425939740799a319f0
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-05-02 16:46:59 +0530

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,11 +1,35 @@
-provider "aws" {
-   region = "${var.region}"
-   profile = "default"
+variable "region" {
+  description = "this gives region description"
+  type = string
 }
 
-resource "aws_vpc" "myvpc" {
-   tags = {
-     Name = "${var.vpcname}"
-   }
-   cidr_block = "${var.vpccidr}"
+variable "vpc_name" {
+  description = "this gives vpc description"
+  type = string
 }
+
+variable "vpc_cidr" {
+  description = "this gives vpc cidr description"
+  type = string
+}
+
+variable "subnetA_name" {
+  description = "this gives Subnet 1 description"
+  type = string
+}
+
+variable "subnetA_cidr" {
+  description = "this gives subnet 1 cidr description"
+  type = string
+}
+
+variable "subnetB_name" {
+  description = "this gives subnet 2 description"
+  type = string
+}
+
+variable "subnetB_cidr" {
+  description = "this gives subnet 2 description"
+  type = string
+}
+
