dbdcac289366b570c763411c35b8a703784dc473
H Sareen Kumar <127433171+Sareenh1@users.noreply.github.com>
2024-05-02 16:28:53 +0530

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -9,19 +9,3 @@ resource "aws_vpc" "myvpc" {
    }
    cidr_block = "${var.vpccidr}"
 }
-
-resource "aws_subnet" "subnet1" {
-   tags = {
-      Name = "${var.subnet1name}"
-   }
-   cidr_block = "${var.subnet1cidr}"
-   vpc_id = "${aws_vpc.myvpc.id}"
-}
-
-resource "aws_subnet" "subnet2" {
-   tags = {
-       Name = "${var.subnet2name}"
-   }
-   cidr_block = "${var.subnet2cidr}"
-   vpc_id = "${aws_vpc.myvpc.id}"
-}
